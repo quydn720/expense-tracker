b696edb8e3bb4db72d646f83af97d94653ae9ba9
Ngọc Quý Đỗ <quydn720@gmail.com>
2021-09-28 00:09:02 +0700

    Consist the color, fix the layout. Need flex size for horizontal

---
+++ lib/pages/splash_page/components/body.dart
@@ -0,0 +1,98 @@
+import '../../../constants.dart';
+import 'custom_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Body extends StatefulWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final data = [
+      {
+        'image': 'assets/images/shopping.svg',
+        'title': 'Retro occupy',
+        'content':
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+      },
+      {
+        'image': 'assets/images/investment.svg',
+        'title': 'Synth polaroid',
+        'content':
+            'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+      },
+      {
+        'image': 'assets/images/payments.svg',
+        'title': 'Keytar sweenet',
+        'content':
+            'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.',
+      },
+    ];
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: SizedBox(
+              width: 280, // TODO: fixed size
+              child: PageView.builder(
+                onPageChanged: (value) => setState(() {
+                  selectedIndex = value;
+                }),
+                itemCount: data.length,
+                itemBuilder: (context, index) => SvgPicture.asset(
+                  data[index]['image'].toString(),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      data.length,
+                      (index) => buildIndicators(index),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Expanded(
+                    child: CustomCard(
+                      title: data[selectedIndex]['title'].toString(),
+                      subtitle: data[selectedIndex]['content'].toString(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  AnimatedContainer buildIndicators(int index) {
+    return AnimatedContainer(
+      margin: const EdgeInsets.only(left: 4.0),
+      height: 8.0,
+      width: index == selectedIndex ? 20.0 : 10.0,
+      decoration: BoxDecoration(
+        color: index == selectedIndex ? kPrimaryColor : Colors.grey,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      duration: Duration(milliseconds: kAnimationDuration),
+    );
+  }
+}
---
+++ lib/pages/splash_page/components/custom_card.dart
@@ -0,0 +1,56 @@
+import '../../../components/default_button.dart';
+import '../../home_pages/home_pages.dart';
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({
+    required this.title,
+    required this.subtitle,
+  });
+  final String title;
+  final String subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 8,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20.0),
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Text(
+              subtitle,
+              textAlign: TextAlign.center,
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Spacer(),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              height: 40.0,
+              width: double.infinity,
+              child: DefaultButton(
+                title: 'I\'m interested',
+                onPressed: () {
+                  Navigator.pushNamed(context, HomePage.routeName);
+                },
+              ),
+            ),
+            const SizedBox(height: 20.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
