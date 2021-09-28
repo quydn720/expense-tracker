8875c61e90c245696a31ec76290d637fcaea2a11
Ngọc Quý Đỗ <quydn720@gmail.com>
2021-09-28 13:14:03 +0700

    Add fonts and style theme

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,4 +3,8 @@ import 'package:flutter/material.dart';
 const Color kPrimaryColor = Color(0xFFFF3378);
 const Color kSecondaryColor = Color(0xFF33C9FF);
 const Color kOptionalColor = Color(0xFF41E94B);
+const Color kWhiteBackgroundColor = Color(0xFFFCFCFC);
+const Color kGreyishColor = Color(0xFFF6F6F7);
 const int kAnimationDuration = 100;
+
+const double kRadius = 20.0;
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:expense_tracker/constants.dart';
-import 'package:expense_tracker/pages/splash_page/splash_page.dart';
-import 'package:expense_tracker/routes.dart';
+import 'package:expense_tracker/pages/daily/daily_page.dart';
+import 'package:expense_tracker/pages/home_pages/home_pages.dart';
+import 'package:expense_tracker/pages/login_pages/login_page.dart';
+
+import 'constants.dart';
+import 'pages/splash_page/splash_page.dart';
+import 'routes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,12 +15,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xFFFCFCFC),
+        scaffoldBackgroundColor: kWhiteBackgroundColor,
         primaryColor: kPrimaryColor,
+        appBarTheme: appBarTheme,
+        textTheme: textTheme,
       ),
-      initialRoute: SplashPage.routeName,
+      initialRoute: HomePage.routeName,
       routes: routes,
     );
   }
 }
+
+const appBarTheme = AppBarTheme(
+  backgroundColor: Colors.white,
+  toolbarHeight: 140.0,
+  shape: const RoundedRectangleBorder(
+    borderRadius: const BorderRadius.all(
+      Radius.circular(kRadius),
+    ),
+  ),
+);
+
+const textTheme = TextTheme(
+  headline1: TextStyle(
+    fontFamily: 'GTWalsheimPro',
+    fontSize: 25.0,
+    fontWeight: FontWeight.bold,
+  ),
+  bodyText2: TextStyle(
+    fontFamily: 'GTWalsheimPro',
+  ),
+  subtitle2: TextStyle(
+    fontFamily: 'GTWalsheimPro',
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+  ),
+  subtitle1: TextStyle(
+    fontFamily: 'GTWalsheimPro',
+    fontSize: 18.0,
+  ),
+);
