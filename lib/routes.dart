6d197adc9b98b16da52b49501d417d01fef8914c
Ngọc Quý Đỗ <quydn720@gmail.com>
2021-10-14 08:45:01 +0700

    Extract more constants

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,20 +1,21 @@
-import 'package:expense_tracker/pages/add_transaction/add_transaction.dart';
-
-import 'pages/home/home_pages.dart';
-import 'pages/login/login_page.dart';
-import 'pages/onboarding/onboarding_page.dart';
-
-import 'pages/splash/splash_page.dart';
-
-import 'pages/daily/daily_page.dart';
-
 import 'package:flutter/material.dart';
 
+import 'presentations/pages/add_transaction/add_transaction.dart';
+import 'presentations/pages/authentication/forgor_password/forgot_pw_page.dart';
+import 'presentations/pages/authentication/sign_in/sign_in_page.dart';
+import 'presentations/pages/authentication/sign_up/sign_up_page.dart';
+import 'presentations/pages/daily/daily_page.dart';
+import 'presentations/pages/home/home_pages.dart';
+import 'presentations/pages/onboarding/onboarding_page.dart';
+import 'presentations/pages/splash/splash_page.dart';
+
 final Map<String, WidgetBuilder> routes = {
-  SplashPage.routeName: (context) => SplashPage(),
-  OnboardingPage.routeName: (context) => OnboardingPage(),
-  HomePage.routeName: (context) => HomePage(),
-  DailyPage.routeName: (context) => DailyPage(),
-  LoginPage.routeName: (context) => LoginPage(),
-  AddNewTransactionPage.routeName: (context) => AddNewTransactionPage(),
+  SplashPage.routeName: (context) => const SplashPage(),
+  OnboardingPage.routeName: (context) => const OnboardingPage(),
+  HomePage.routeName: (context) => const HomePage(),
+  DailyPage.routeName: (context) => const DailyPage(),
+  AddNewTransactionPage.routeName: (context) => const AddNewTransactionPage(),
+  SignInPage.routeName: (context) => const SignInPage(),
+  SignUpPage.routeName: (context) => const SignUpPage(),
+  ForgotPasswordPage.routeName: (context) => const ForgotPasswordPage(),
 };
