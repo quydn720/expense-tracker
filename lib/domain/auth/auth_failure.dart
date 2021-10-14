1b2c3bf7dd67ba445b20577d86903999baf07e36
Ngọc Quý Đỗ <quydn720@gmail.com>
2021-10-14 08:35:31 +0700

    Adding the domain & infastructure layer for authenticate

---
+++ lib/domain/auth/auth_failure.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'auth_failure.freezed.dart';
+
+@freezed
+class AuthFailure with _$AuthFailure {
+  const factory AuthFailure.cancelledByUser() = CancelledByUser;
+  // Serves as a "catch all" failure if we don't know what exactly went wrong
+  const factory AuthFailure.serverError() = ServerError;
+  const factory AuthFailure.emailAlreadyInUse() = EmailAlreadyInUse;
+  const factory AuthFailure.invalidEmailAndPasswordCombination() =
+      InvalidEmailAndPasswordCombination;
+}
---
+++ lib/domain/core/value_object.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+
+import 'errors.dart';
+import 'failures.dart';
+
+@immutable
+abstract class ValueObject<T> {
+  const ValueObject();
+  Either<ValueFailure<T>, T> get value;
+
+  /// Throws [UnexpectedValueError] containing the [ValueFailure]
+  T getOrCrash() {
+    // id = identity - same as writing (right) => right
+    return value.fold((f) => throw UnexpectedValueError(f), id);
+  }
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+    return o is ValueObject<T> && o.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'Value($value)';
+
+  bool isValid() => value.isRight();
+}
