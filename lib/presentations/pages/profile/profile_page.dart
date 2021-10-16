e3fdbe14b40063a100ad7273852feecc9bd038ba
Ngọc Quý Đỗ <quydn720@gmail.com>
2021-10-16 16:49:20 +0700

    Add new profile page

--- lib/presentations/pages/profile/profile_page.dart
+++ lib/presentations/pages/profile/profile_page.dart
@@ -1,81 +1,127 @@
+import 'package:expense_tracker/presentations/components/icon_card.dart';
 import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
-import '../../../size_config.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage();
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Body();
+    return const Body();
   }
 }
 
 class Body extends StatelessWidget {
-  const Body();
-
+  const Body({Key? key}) : super(key: key);
+  final data = const [
+    {'img': 'assets/icons/wallet-3.png', 'title': 'Account'},
+    {'img': 'assets/icons/settings.png', 'title': 'Settings'},
+    {'img': 'assets/icons/download.png', 'title': 'Export Data'},
+    {'img': 'assets/icons/logout.png', 'title': 'Logout'},
+  ];
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 35.0),
-      child: Column(
-        children: [
-          Spacer(),
-          Expanded(
-            flex: 3,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(kMediumPadding),
+        child: Column(
+          children: [
+            Row(
               children: [
-                CircleAvatar(
-                  child: Text('PT'),
-                  radius: SizeConfig.screenWidth * 0.125,
+                const CircleAvatar(radius: 45),
+                const SizedBox(width: kMediumPadding),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Username', style: body3),
+                      Text('Ngoc Quy', style: title2.copyWith(color: kDark75)),
+                    ],
+                  ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Pham Quoc Trung',
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                    Text('Credit score: 73.50'),
-                  ],
+                IconButton(
+                  icon: Image.asset('assets/icons/edit.png'),
+                  onPressed: () {},
                 ),
               ],
             ),
-          ),
-          Spacer(),
-          Container(
-            decoration: BoxDecoration(
-              color: kPrimaryColor,
-              borderRadius: const BorderRadius.all(
-                Radius.circular(kDefaultPadding),
+            const SizedBox(height: 40),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24.0),
+              ),
+              child: ListView.separated(
+                shrinkWrap: true,
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(kMediumPadding),
+                    child: Row(
+                      children: [
+                        SquaredIconCard(
+                          imagePath: data[index]['img']!,
+                          height: 60,
+                          imageColor: index != 3 ? kViolet100 : kRed100,
+                          color: index != 3 ? kViolet20 : kRed20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: kMediumPadding),
+                          child: Text(
+                            data[index]['title']!,
+                            style: title3.copyWith(color: kDark75),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => const Divider(),
               ),
             ),
-            width: double.infinity,
-            height: SizeConfig.screenHeight * 0.125,
-          ),
-          Spacer(),
-          Divider(),
-          ListTile(
-            title: Text('Email'),
-            subtitle: Text('sasukekun@gmail.com'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Date of birth'),
-            subtitle: Text('sasukekun@gmail.com'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Email'),
-            subtitle: Text('sasukekun@gmail.com'),
-          ),
-          Spacer(),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
+// Column(
+//   children: const [
+//     ListTile(
+//       leading: SquaredIconCard(
+//         imagePath: 'assets/icons/wallet-3.png',
+//         height: 60,
+//         imageColor: kViolet100,
+//         color: kViolet20,
+//       ),
+//       title: Text('Account'),
+//     ),
+//     ListTile(
+//       leading: SquaredIconCard(
+//         imagePath: 'assets/icons/wallet-3.png',
+//         height: 60,
+//         imageColor: kViolet100,
+//         color: kViolet20,
+//       ),
+//       title: Text('Account'),
+//     ),
+//     ListTile(
+//       leading: SquaredIconCard(
+//         imagePath: 'assets/icons/wallet-3.png',
+//         height: 60,
+//         imageColor: kViolet100,
+//         color: kViolet20,
+//       ),
+//       title: Text('Account'),
+//     ),
+//     ListTile(
+//       leading: SquaredIconCard(
+//         imagePath: 'assets/icons/wallet-3.png',
+//         height: 60,
+//         imageColor: kViolet100,
+//         color: kViolet20,
+//       ),
+//       title: Text('Account'),
+//     ),
+//   ],
+// ),
