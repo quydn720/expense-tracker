fb1ad9a662de1238208e4720bfca8b740039fa26
Quy Do Ngoc <quydn720@gmail.com>
2022-09-09 10:07:22 +0700

    [di] inject initial location into router

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/features/authentication/presentation/pages/register_screen.dart';
 import 'package:expense_tracker/features/settings/presentation/pages/currency_screen.dart';
 import 'package:expense_tracker/features/settings/presentation/pages/setting_screen.dart';
 import 'package:expense_tracker/home_screen.dart';
@@ -14,75 +15,92 @@ import 'fade_transistion_page.dart';
 
 const ValueKey<String> _scaffoldKey = ValueKey<String>('App scaffold');
 
-final router = GoRouter(
-  routes: <GoRoute>[
-    GoRoute(path: '/', redirect: (_) => '/home'),
-    GoRoute(
-      path: '/home',
-      pageBuilder: (context, state) => FadeTransitionPage(
-        child: const AppScaffold(
-          selectedTab: ScaffoldTab.home,
-          child: HomeScreen(),
-        ),
-        key: _scaffoldKey,
-      ),
-    ),
-    GoRoute(
-      path: '/transaction',
-      pageBuilder: (context, state) => FadeTransitionPage(
-        child: const AppScaffold(
-          selectedTab: ScaffoldTab.transaction,
-          child: HomeScreen(),
-        ),
-        key: _scaffoldKey,
-      ),
-    ),
-    GoRoute(
-      path: '/budget',
-      pageBuilder: (context, state) => FadeTransitionPage(
-        child: const AppScaffold(
-          selectedTab: ScaffoldTab.budget,
-          child: HomeScreen(),
-        ),
-        key: _scaffoldKey,
-      ),
-    ),
-    GoRoute(
-      path: '/profile',
-      redirect: (state) => '/setting',
-    ),
-    GoRoute(
-      path: '/setting',
-      pageBuilder: (context, state) => FadeTransitionPage(
-        key: _scaffoldKey,
-        child: const AppScaffold(
-          selectedTab: ScaffoldTab.profile,
-          child: SettingScreen(),
-        ),
-      ),
-      routes: [
+GoRouter router([String? initialLocation]) => GoRouter(
+      initialLocation: initialLocation,
+      routes: <GoRoute>[
+        GoRoute(path: '/', redirect: (_) => '/home'),
         GoRoute(
-          path: 'currency',
-          builder: (context, state) => const CurrencyScreen(),
+          path: '/home',
+          pageBuilder: (context, state) => FadeTransitionPage(
+            child: const AppScaffold(
+              selectedTab: ScaffoldTab.home,
+              child: HomeScreen(),
+            ),
+            key: _scaffoldKey,
+          ),
         ),
         GoRoute(
-          path: 'language',
-          builder: (context, state) => const LanguageScreen(),
+          path: '/transaction',
+          pageBuilder: (context, state) => FadeTransitionPage(
+            child: const AppScaffold(
+              selectedTab: ScaffoldTab.transaction,
+              child: HomeScreen(),
+            ),
+            key: _scaffoldKey,
+          ),
         ),
         GoRoute(
-          path: 'theme',
-          builder: (context, state) => const ThemeScreen(),
+          path: '/budget',
+          pageBuilder: (context, state) => FadeTransitionPage(
+            child: const AppScaffold(
+              selectedTab: ScaffoldTab.budget,
+              child: HomeScreen(),
+            ),
+            key: _scaffoldKey,
+          ),
         ),
         GoRoute(
-          path: 'security',
-          builder: (context, state) => const SecurityScreen(),
+          path: '/profile',
+          redirect: (state) => '/setting',
         ),
         GoRoute(
-          path: 'notification',
-          builder: (context, state) => const NotificationScreen(),
+          path: '/setting',
+          pageBuilder: (context, state) => FadeTransitionPage(
+            key: _scaffoldKey,
+            child: const AppScaffold(
+              selectedTab: ScaffoldTab.profile,
+              child: SettingScreen(),
+            ),
+          ),
+          routes: [
+            GoRoute(
+              path: 'currency',
+              builder: (context, state) => const CurrencyScreen(),
+            ),
+            GoRoute(
+              path: 'language',
+              builder: (context, state) => const LanguageScreen(),
+            ),
+            GoRoute(
+              path: 'theme',
+              builder: (context, state) => const ThemeScreen(),
+            ),
+            GoRoute(
+              path: 'security',
+              builder: (context, state) => const SecurityScreen(),
+            ),
+            GoRoute(
+              path: 'notification',
+              builder: (context, state) => const NotificationScreen(),
+            ),
+          ],
+        ),
+        GoRoute(
+          path: '/register',
+          builder: (_, __) => const RegisterProvider(),
+        ),
+        GoRoute(
+          path: '/dev_view',
+          builder: (_, __) => const AppDevelopmentView(),
         ),
       ],
-    ),
-  ],
-  debugLogDiagnostics: true,
-);
+      debugLogDiagnostics: true,
+    );
+
+class AppDevelopmentView extends StatelessWidget {
+  const AppDevelopmentView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const RegisterProvider();
+  }
+}
