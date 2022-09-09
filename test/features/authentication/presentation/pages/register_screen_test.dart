53af0622e36dc4deaa9f30871311c7285de99ace
Quy Do Ngoc <quydn720@gmail.com>
2022-09-09 10:07:31 +0700

    [add]

--- test/features/authentication/presentation/pages/register_screen_test.dart
+++ test/features/authentication/presentation/pages/register_screen_test.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/features/authentication/domain/usecases/register_with_email_and_pw.dart';
 import 'package:expense_tracker/features/authentication/presentation/pages/cubit/register_cubit.dart';
+import 'package:expense_tracker/features/authentication/presentation/pages/register_form.dart';
 import 'package:expense_tracker/features/authentication/presentation/pages/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,14 +16,19 @@ class MockGoRouter extends Mock implements GoRouter {}
 
 class MockRegisterCubit extends Mock implements RegisterCubit {}
 
-GoRouter routers([String? initialLocation, RegisterCubit? registerCubit]) =>
+class MockNormalText extends Mock implements NormalText {}
+
+GoRouter mockRouter({
+  String? initialLocation,
+  required RegisterCubit registerCubit,
+}) =>
     GoRouter(
       initialLocation: initialLocation ?? 'home',
       routes: [
         GoRoute(
-          path: '/h',
+          path: '/register',
           builder: (_, __) => BlocProvider.value(
-            value: registerCubit!,
+            value: registerCubit,
             child: const RegisterScreen(),
           ),
         ),
@@ -32,63 +38,116 @@ GoRouter routers([String? initialLocation, RegisterCubit? registerCubit]) =>
         )
       ],
     );
+
 void main() {
   late RegisterCubit registerCubit;
+  const nameInputFieldKey = Key('name_input_field');
 
   setUp(() {
     registerCubit = MockRegisterCubit();
+
     when(() => registerCubit.stream).thenAnswer((_) => const Stream.empty());
     when(() => registerCubit.state).thenAnswer((_) => const RegisterState());
   });
 
   testWidgets('renders RegisterScreen', (tester) async {
-    await tester.pumpWidget(
-      MaterialApp.router(
-        routeInformationParser: routers(
-          '/h',
-          registerCubit,
-        ).routeInformationParser,
-        routerDelegate: routers(
-          '/h',
-          registerCubit,
-        ).routerDelegate,
-        routeInformationProvider: routers(
-          '/h',
-          registerCubit,
-        ).routeInformationProvider,
-      ),
-    );
+    await tester.pumpWidget(materialApp(registerCubit));
 
     expect(find.byType(RegisterScreen), findsOneWidget);
   });
-  testWidgets('invokes bloc.onButtonSubmit', (tester) async {
-    when(() => registerCubit.stream).thenAnswer((_) => const Stream.empty());
-    when(() => registerCubit.state).thenReturn(
-      const RegisterState(status: FormzStatus.valid),
-    );
+  testWidgets(
+    'invokes bloc.onButtonSubmit',
+    (tester) async {
+      when(() => registerCubit.stream).thenAnswer((_) => const Stream.empty());
+      when(() => registerCubit.state).thenReturn(
+        const RegisterState(status: FormzStatus.valid),
+      );
 
-    await tester.pumpWidget(
-      MaterialApp.router(
-        routeInformationParser:
-            routers('/h', registerCubit).routeInformationParser,
-        routerDelegate: routers('/h', registerCubit).routerDelegate,
-        routeInformationProvider:
-            routers('/h', registerCubit).routeInformationProvider,
-      ),
-    );
+      await tester.pumpWidget(materialApp(registerCubit));
 
-    await tester.enterText(find.byKey(const Key('name_input_field')), 'name');
-    await tester.enterText(find.byKey(const Key('email_input_field')), 'email');
-    await tester.enterText(find.byKey(const Key('pw_input_field')), 'pw');
+      expect(find.text('Button'), findsOneWidget);
+      await tester.tap(find.text('Button'));
+      await tester.pumpAndSettle();
 
-    expect(find.byKey(const Key('name_input_field')), findsOneWidget);
-    expect(find.text('Button'), findsOneWidget);
-    await tester.tap(find.text('Button'));
-    await tester.pumpAndSettle();
+      verify(registerCubit.onButtonClicked).called(1);
+    },
+  );
 
-    verify(() => registerCubit.onEmailChanged('email')).called(1);
-    verify(() => registerCubit.onNameChanged('name')).called(1);
-    verify(() => registerCubit.onPasswordChanged('pw')).called(1);
-    verify(registerCubit.onButtonClicked).called(1);
+  testWidgets('button not enable when status is invalid', (tester) async {
+    await tester.pumpWidget(materialApp(registerCubit));
+    final buttonFinder = find.byKey(const Key('register_button'));
+    final registerButton = tester.widget<ElevatedButton>(buttonFinder);
+    expect(registerButton.enabled, isFalse);
   });
+
+  testWidgets('show invalid error string', (tester) async {
+    final name = MockNormalText();
+    when(() => name.invalid).thenReturn(true);
+    when(() => name.error).thenReturn(NormalTextError.invalid);
+    when(() => registerCubit.state).thenReturn(RegisterState(name: name));
+
+    await tester.pumpWidget(materialApp(registerCubit));
+
+    final errorValidationString = find.text('Invalid name');
+    expect(errorValidationString, findsOneWidget);
+  });
+  testWidgets('show empty error string', (tester) async {
+    final name = MockNormalText();
+    when(() => name.invalid).thenReturn(true);
+    when(() => name.error).thenReturn(NormalTextError.empty);
+    when(() => registerCubit.state).thenReturn(RegisterState(name: name));
+
+    await tester.pumpWidget(materialApp(registerCubit));
+
+    final errorValidationString = find.text("Name can't be empty");
+    expect(errorValidationString, findsOneWidget);
+  });
+  testWidgets('show empty error string', (tester) async {
+    final name = MockNormalText();
+    when(() => name.invalid).thenReturn(true);
+    when(() => name.error).thenReturn(null);
+    await tester.pumpWidget(materialApp(registerCubit));
+    expect(name.error, isNull);
+  });
+
+  testWidgets(
+    'change true state when type in the input field',
+    (tester) async {
+      when(() => registerCubit.stream).thenAnswer((_) => const Stream.empty());
+      when(() => registerCubit.state).thenReturn(
+        const RegisterState(status: FormzStatus.valid),
+      );
+
+      await tester.pumpWidget(materialApp(registerCubit));
+
+      await tester.enterText(find.byKey(nameInputFieldKey), 'name');
+      verify(() => registerCubit.onNameChanged('name')).called(1);
+
+      await tester.enterText(
+        find.byKey(const Key('email_input_field')),
+        'email',
+      );
+      verify(() => registerCubit.onEmailChanged('email')).called(1);
+
+      await tester.enterText(find.byKey(const Key('pw_input_field')), 'pw');
+      verify(() => registerCubit.onPasswordChanged('pw')).called(1);
+    },
+  );
+}
+
+MaterialApp materialApp(RegisterCubit registerCubit) {
+  return MaterialApp.router(
+    routeInformationParser: mockRouter(
+      initialLocation: '/register',
+      registerCubit: registerCubit,
+    ).routeInformationParser,
+    routerDelegate: mockRouter(
+      initialLocation: '/register',
+      registerCubit: registerCubit,
+    ).routerDelegate,
+    routeInformationProvider: mockRouter(
+      initialLocation: '/register',
+      registerCubit: registerCubit,
+    ).routeInformationProvider,
+  );
 }
