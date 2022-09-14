4f77b753a8d01b116a2061363346a5f753487afd
Quy Do Ngoc <quydn720@gmail.com>
2022-09-14 14:41:44 +0700

    [format]

--- lib/presentations/pages/main/components/custom_bottom_nav_item.dart
+++ lib/presentations/pages/main/components/custom_bottom_nav_item.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-
 class CustomBottomNavItem extends StatelessWidget {
   const CustomBottomNavItem({
     required this.icon,
