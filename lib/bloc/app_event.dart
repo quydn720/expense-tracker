f712ad05a6d6c3166712fc0ef44efe7968dd3377
Quy Do Ngoc <quydn720@gmail.com>
2022-09-15 16:09:17 +0700

    [add] complted onboarding & verify email

--- lib/bloc/app_event.dart
+++ lib/bloc/app_event.dart
@@ -3,5 +3,7 @@ part of 'app_bloc.dart';
 @freezed
 class AppEvent with _$AppEvent {
   const factory AppEvent.onUserChanged() = OnUserChanged;
+  const factory AppEvent.onEmailVerified() = OnEmailVerified;
   const factory AppEvent.logoutRequested() = LogoutRequested;
+  const factory AppEvent.onboardingCompleted() = OnboardingCompleted;
 }
