3bebc2cb28a8078b750ada6124b99145f28f7a20
Quy Do Ngoc <quydn720@gmail.com>
2022-09-18 09:00:03 +0700

    [fix] test with email waiting state

--- test/bloc/app_bloc_test.dart
+++ test/bloc/app_bloc_test.dart
@@ -19,15 +19,48 @@ void main() {
       when(() => authenticationRepository.user).thenAnswer(
         (_) => const Stream.empty(),
       );
-      when(
-        () => authenticationRepository.currentUser,
-      ).thenReturn(User.empty);
+      when(() => authenticationRepository.currentUser).thenReturn(User.empty);
     });
+
+    test(
+      '''initial value is [Initial] when user (have NOT) completed onboarding''',
+      () async {
+        expect(
+          AppBloc(authenticationRepository: authenticationRepository).state,
+          const Initial(),
+        );
+      },
+    );
+    test(
+      '''initial value is [Initial] when user have (completed) onboarding''',
+      () async {
+        expect(
+          AppBloc(
+            authenticationRepository: authenticationRepository,
+            showOnboarding: false,
+          ).state,
+          const Unauthenticated(),
+        );
+      },
+    );
     blocTest<AppBloc, AppState>(
-      'emits authenticated when user is not empty',
+      'emits waitForEmailVerification when user.verify is false',
       setUp: () {
         when(() => user.isNotEmpty).thenReturn(true);
-        when(() => user.id).thenReturn('ABC');
+        when(() => user.verified).thenReturn(false);
+        when(() => authenticationRepository.user).thenAnswer(
+          (_) => Stream.value(user),
+        );
+      },
+      act: (bloc) => bloc.add(OnUserChanged(user)),
+      build: () => AppBloc(authenticationRepository: authenticationRepository),
+      seed: AppState.unauthenticated,
+      expect: () => [const AppState.waitForEmailVerification()],
+    );
+    blocTest<AppBloc, AppState>(
+      'emits authenticated when user is verified',
+      setUp: () {
+        when(() => user.verified).thenReturn(true);
         when(() => authenticationRepository.user).thenAnswer(
           (_) => Stream.value(user),
         );
@@ -45,7 +78,18 @@ void main() {
         );
       },
       build: () => AppBloc(authenticationRepository: authenticationRepository),
-      expect: () => [const AppState.authenticated()],
+      expect: () => [const AppState.unauthenticated()],
+    );
+    blocTest<AppBloc, AppState>(
+      'emits unauthenticated when LogOutRequested',
+      setUp: () {
+        when(() => authenticationRepository.user).thenAnswer(
+          (_) => Stream.value(User.empty),
+        );
+      },
+      act: (bloc) => bloc.add(const LogoutRequested()),
+      build: () => AppBloc(authenticationRepository: authenticationRepository),
+      expect: () => [const AppState.unauthenticated()],
     );
   });
 }
