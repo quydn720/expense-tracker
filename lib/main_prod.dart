8b062cc304f4b66c4b4b1ee35d581fd74c1e1a08
Quy Do Ngoc <quydn720@gmail.com>
2022-09-18 08:59:00 +0700

    [ref] update firebase initilization

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,19 +1,32 @@
+import 'package:expense_tracker/theme_controller.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+import 'package:provider/provider.dart';
 
 import 'app.dart';
+import 'common/cache/local_cache.dart';
 import 'di/injector.dart';
+import 'locale_controller.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // TODO(quy): try presolve
-  // await Firebase.initializeApp();
+  await Firebase.initializeApp();
 
   await configureInjection(Environment.prod);
 
-  // final authenticationRepository = AuthenticateRepo();
-  // await authenticationRepository.user.first;
-
-  runApp(const App());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ThemeController(getIt<ILocalCache>()),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => LocaleController(getIt<ILocalCache>()),
+        ),
+      ],
+      child: const App(),
+    ),
+  );
 }
