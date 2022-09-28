886fbe06422f00e56a37400febae876227869118
Quy Do Ngoc <quydn720@gmail.com>
2022-09-28 14:20:48 +0700

    [ref] fix

--- lib/theme_controller.dart
+++ lib/theme_controller.dart
@@ -4,9 +4,10 @@ import 'package:flutter/material.dart';
 import 'di/injector.dart';
 
 class ThemeController extends ChangeNotifier {
-  ThemeController(this._localCache)
-      : _themeMode =
-            _localCache.get<ThemeMode?>('theme-mode-key') ?? ThemeMode.system;
+  ThemeController(this._localCache) {
+    _themeMode =
+        _localCache.get<ThemeMode?>('theme-mode-key') ?? ThemeMode.system;
+  }
 
   late ThemeMode _themeMode;
   final ILocalCache _localCache;
