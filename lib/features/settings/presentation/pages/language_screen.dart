0c657ccdf773f56b84d7164ed34f30db8efcadfd
Quy Do Ngoc <quydn720@gmail.com>
2022-09-28 14:19:58 +0700

    [ref] show the current selected language

--- lib/features/settings/presentation/pages/language_screen.dart
+++ lib/features/settings/presentation/pages/language_screen.dart
@@ -1,4 +1,6 @@
+import 'package:expense_tracker/features/settings/domain/entities/language.dart';
 import 'package:expense_tracker/gen/assets.gen.dart';
+import 'package:expense_tracker/l10n/localization_factory.dart';
 import 'package:expense_tracker/locale_controller.dart';
 import 'package:expense_tracker/presentations/components/default_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -13,31 +15,32 @@ class LanguageScreen extends StatelessWidget {
     final textStyle = Theme.of(context).textTheme.subtitle2?.copyWith(
           color: const Color(0xff0D0E0F),
         );
+    final currentLang = context.read<LocaleController>().locale;
 
     return Scaffold(
-      appBar: const DefaultAppBar(title: 'Language'),
+      appBar: DefaultAppBar(title: context.l10n.language),
       body: ListView.builder(
-        itemBuilder: (_, index) => ListTile(
-          onTap: () => context.read<LocaleController>().changeLocale(
-                const Locale('vi'),
-              ),
-          title: Text(_languages[index]['name']!, style: textStyle),
-          minVerticalPadding: 17,
-          trailing: Assets.icons.success.svg(
-            color: Theme.of(context).primaryColor,
-          ),
-        ),
-        itemCount: _languages.length,
+        itemBuilder: (_, index) {
+          final language = Language.supportedLanguages[index];
+          final controller = context.read<LocaleController>();
+          return ListTile(
+            onTap: () {
+              final locale = Locale.fromSubtags(
+                languageCode: language.languageCode,
+              );
+              controller.changeLocale(locale);
+            },
+            title: Text(language.country, style: textStyle),
+            minVerticalPadding: 17,
+            trailing: currentLang.languageCode == language.languageCode
+                ? Assets.icons.success.svg(
+                    color: Theme.of(context).primaryColor,
+                  )
+                : const SizedBox.shrink(),
+          );
+        },
+        itemCount: Language.supportedLanguages.length,
       ),
     );
   }
 }
-
-final _languages = [
-  {'name': 'English'},
-  {'name': 'Vietnamese'},
-  {'name': 'Indonesian'},
-  {'name': 'Japanese'},
-  {'name': 'Russian'},
-  {'name': 'Korean'},
-];
