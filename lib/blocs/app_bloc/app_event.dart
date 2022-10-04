817c7a4a819eff1ecb56361e34293f40d20b5652
Quy Do Ngoc <quydn720@gmail.com>
2022-10-04 10:21:28 +0700

    [remove] old blocs

--- lib/blocs/app_bloc/app_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'app_bloc.dart';
-
-@freezed
-class AppEvent with _$AppEvent {
-  const factory AppEvent.appStarted() = AppStarted;
-  const factory AppEvent.appLogOutRequested() = AppLogOutRequested;
-  const factory AppEvent.appUserChanged(User user) = AppUserChanged;
-  const factory AppEvent.userChangeAvatar(String avt) = UserChangeAvatar;
-}
