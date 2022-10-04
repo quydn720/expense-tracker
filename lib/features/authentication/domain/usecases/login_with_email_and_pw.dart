a59fe16d54192d44188936a26c9cff294dfc0374
Quy Do Ngoc <quydn720@gmail.com>
2022-10-04 10:35:59 +0700

    [add] use case with dartz

--- lib/features/authentication/domain/usecases/login_with_email_and_pw.dart
+++ lib/features/authentication/domain/usecases/login_with_email_and_pw.dart
@@ -1,1 +1,30 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:injectable/injectable.dart';
 
+@injectable
+class LoginWithEmailAndPwUseCase {
+  LoginWithEmailAndPwUseCase({
+    required IAuthenticationRepository auth,
+  }) : _auth = auth;
+
+  final IAuthenticationRepository _auth;
+
+  Future<Either<LogInWithEmailAndPasswordFailure, Unit>> call({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _auth.logInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return right(unit);
+    } on FirebaseAuthException catch (e) {
+      return left(LogInWithEmailAndPasswordFailure.fromCode(e.code));
+    } catch (_) {
+      return left(const LogInWithEmailAndPasswordFailure());
+    }
+  }
+}
