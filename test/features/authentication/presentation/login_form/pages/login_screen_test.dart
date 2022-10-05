09316f18455879415635614789cd236d4921e55a
Quy Do Ngoc <quydn720@gmail.com>
2022-10-05 15:35:20 +0700

    [fix] update tests on login screen

--- test/features/authentication/presentation/login_form/pages/login_screen_test.dart
+++ test/features/authentication/presentation/login_form/pages/login_screen_test.dart
@@ -1,8 +1,10 @@
 import 'package:expense_tracker/features/authentication/domain/usecases/login_with_email_and_pw.dart';
+import 'package:expense_tracker/features/authentication/presentation/login_form/cubit/login_form_cubit.dart';
 import 'package:expense_tracker/features/authentication/presentation/login_form/pages/login_screen.dart';
 import 'package:expense_tracker/features/settings/theme/theme_controller.dart';
 import 'package:expense_tracker/l10n/locale_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
@@ -13,21 +15,24 @@ import '../../../../../../test_helper/app_test.dart';
 import '../../../../../../test_helper/mock_router.dart';
 import '../../../../../../test_helper/mocks.dart';
 
+class MockLoginFormCubit extends Mock implements LoginFormCubit {}
+
 void main() {
   late LocaleController localeController;
   late ThemeController themeController;
+  late LoginFormCubit loginFormCubit;
   late GoRouter router;
 
   setUp(() {
-    GetIt.instance.registerSingleton<LoginWithEmailAndPwUseCase>(
-      MockLoginWithEmailAndPwUseCase(),
-    );
-
     router = mockRouter(
       testingRoute: [
-        GoRoute(path: '/', builder: (_, __) => const LoginProvider())
+        GoRoute(path: '/', builder: (_, __) => const LoginScreen())
       ],
     );
+
+    loginFormCubit = MockLoginFormCubit();
+    when(() => loginFormCubit.stream).thenAnswer((_) => const Stream.empty());
+    when(() => loginFormCubit.state).thenReturn(const LoginFormState());
 
     localeController = MockLocaleController();
     when(() => localeController.locale).thenReturn(const Locale('en'));
@@ -40,7 +45,8 @@ void main() {
       TestApp(
         providers: [
           ChangeNotifierProvider.value(value: localeController),
-          ChangeNotifierProvider.value(value: themeController)
+          ChangeNotifierProvider.value(value: themeController),
+          BlocProvider.value(value: loginFormCubit),
         ],
         router: router,
       ),
