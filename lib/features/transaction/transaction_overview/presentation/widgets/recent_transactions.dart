aa0d97aed0de2eee94583627bc31506e9b7f1dd9
Quy Do Ngoc <quydn720@gmail.com>
2022-12-30 14:32:46 +0700

    extract widget

--- lib/features/transaction/transaction_overview/presentation/widgets/recent_transactions.dart
+++ lib/features/transaction/transaction_overview/presentation/widgets/recent_transactions.dart
@@ -1,35 +1,65 @@
-import 'package:expense_tracker/features/transaction/presentation/widgets/transaction_tile.dart';
-import 'package:expense_tracker/features/transaction/transaction_overview/presentation/bloc/transaction_bloc.dart';
+import 'package:expense_tracker/l10n/localization_factory.dart';
+import 'package:expense_tracker/routes/app_scaffold.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+
+const seeAllTransactionButtonKey = Key('homeScreen_seeAll_button');
 
 class RecentTransactions extends StatelessWidget {
   const RecentTransactions({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TransactionBloc, TransactionState>(
-      builder: (context, state) {
-        if (state.status != TransactionStatus.loaded) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        return SizedBox(
-          height: 400,
-          child: ListView(
+    final textTheme = Theme.of(context).textTheme;
+    final l10n = context.l10n;
+    return Column(
+      children: [
+        SizedBox(
+          height: 54,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              for (final transaction in state.transactions)
-                TransactionTile(
-                  transaction: transaction,
-                  onPress: () => context.push(
-                    '/transactions/${transaction.id}',
-                    extra: transaction,
+              Text(l10n.recent_transactions, style: textTheme.headline6),
+              GestureDetector(
+                key: seeAllTransactionButtonKey,
+                onTap: () {
+                  context.go('/${ScaffoldTab.transactions.name}');
+                },
+                child: Chip(
+                  label: Text(
+                    l10n.see_all,
+                    style: textTheme.bodyText2?.copyWith(
+                      color: Theme.of(context).primaryColor,
+                    ),
                   ),
-                )
+                ),
+              ),
             ],
           ),
-        );
-      },
+        ),
+        // BlocBuilder<TransactionBloc, TransactionState>(
+        //   builder: (context, state) {
+        //     if (state.status != TransactionStatus.loaded) {
+        //       return const Center(child: CircularProgressIndicator());
+        //     }
+        //     return SizedBox(
+        //       height: 400,
+        //       child: ListView(
+        //         children: [
+        //           for (final transaction in state.transactions)
+        //             TransactionTile(
+        //               transaction: transaction,
+        //               onPress: () => context.push(
+        //                 '/transactions/${transaction.id}',
+        //                 extra: transaction,
+        //               ),
+        //             )
+        //         ],
+        //       ),
+        //     );
+        //   },
+        // ),
+      ],
     );
   }
 }
